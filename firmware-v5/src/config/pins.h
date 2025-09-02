6a9e65c2e5915b8c19ae5733305408dae5aa11af
RaizGp <s22004296@ousl.lk>
2025-09-02 13:03:51 +0530

    Update motor and encoder pin assignments
    Reassigned motor, PWM, encoder, and TX/RX pins to new values in pins.h. This change likely reflects a hardware revision or wiring update to match the current board layout.

--- firmware-v5/src/config/pins.h
+++ firmware-v5/src/config/pins.h
@@ -4,23 +4,23 @@
 #define PIN_LED_INBUILT 47
 
 // --------------------------------------------------------- Motors and Encoders
-#define PIN_MOT_R1 43
-#define PIN_MOT_R2 33
-#define PIN_MOT_L1 44
-#define PIN_MOT_L2 37
+#define PIN_MOT_R1 8
+#define PIN_MOT_R2 37
+#define PIN_MOT_L1 7
+#define PIN_MOT_L2 6
 
-#define PIN_PWM_R 12
-#define PIN_PWM_L 13
+#define PIN_PWM_R 37
+#define PIN_PWM_L 5
 
-#define PIN_ENC_RA 36
-#define PIN_ENC_RB 38
+#define PIN_ENC_RA 43
+#define PIN_ENC_RB 44
 
-#define PIN_ENC_LA 34
-#define PIN_ENC_LB 35
+#define PIN_ENC_LA 4
+#define PIN_ENC_LB 2
 
 // ----------------------------------------------------- Unused or reserved pins
-#define PIN_TX 1
-#define PIN_RX 3
+#define PIN_TX 43
+#define PIN_RX 44
 
 #define PIN_SCL 22
 #define PIN_SDA 21
